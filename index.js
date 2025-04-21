490ffb098c86830f97d7facfc4e5f047acf05a07
aquasquirel <aquasquirel@gmail.com>
2025-04-21 20:42:04 -0300

    Adicionado novos parametros para agendamento.

--- index.js
+++ index.js
@@ -65,6 +65,18 @@ function ordenarAgendamentosPorData(agendamentos) {
     });
 }
 
+function formatarNome(name) {
+    if (!name) return "";
+    
+    return name
+      .toLowerCase()                // deixa tudo minúsculo primeiro
+      .split(' ')                   // separa por espaço
+      .map(word => 
+        word.charAt(0).toUpperCase() + word.slice(1)
+      )                              // coloca primeira letra maiúscula em cada palavra
+      .join(' ');                    // junta tudo de novo
+  }
+
 function mandarAgenda(client, message, user){
            // Ordena os agendamentos antes de enviar
            const agendamentosOrdenados = ordenarAgendamentosPorData(user.agendamentos);
@@ -72,7 +84,7 @@ function mandarAgenda(client, message, user){
            // Enviar os agendamentos ordenados
            let resposta = '**Agendamentos:**\n\n';
            agendamentosOrdenados.forEach((agendamento, index) => {
-               resposta += `${index + 1}. *Destino:* ${agendamento.destino} - *Data:* ${agendamento.data} \n\n`;
+               resposta += `${index + 1}. *Destino:* ${agendamento.destino} \n-*Data:* ${agendamento.data} \n\n`;
            });
 
            client.sendMessage(message.from, resposta);
@@ -105,23 +117,58 @@ client.on('message_create', message => {
         return;
     }
 
+    if(user.andamento == 0 && !isNaN(msg)){
+        //envia os detalhes da agenda expecifica de acordo com o index]
+        const index = parseInt(msg) - 1; // Ajusta para o índice correto
+        if (index >= 0 && index < user.agendamentos.length) {
+            const agendamento = user.agendamentos[index];
+            client.sendMessage(message.from, `*Destino:* ${agendamento.destino}\n\n- *Data:* ${agendamento.data}\n\n- *Contratante:* ${agendamento.contratante}\n\n- *Valor:* ${agendamento.valor}\n\n- *Passageiros:* ${agendamento.passageiros}`);
+        } else {
+            client.sendMessage(message.from, 'Agendamento não encontrado.');
+        }
+        return;
+    }
+
     if (user.andamento === 1) {
-        user.registroTemp.destino = originalMsg; // Usar a resposta original
+        user.registroTemp.destino = formatarNome(originalMsg);
         user.andamento = 2;
         client.sendMessage(message.from, 'Qual a data? (Formato: dia/mês)');
         return;
     }
 
-    // Quando o usuário envia a data
     if (user.andamento === 2) {
         user.registroTemp.data = originalMsg; // Usar a resposta original
+        user.andamento = 3;
+        client.sendMessage(message.from, 'Qual o contratante?');
+        return;
+    }
+
+    if (user.andamento === 3) {
+        user.registroTemp.contratante = formatarNome(originalMsg); // Usar a resposta original
+        user.andamento = 4;
+        client.sendMessage(message.from, 'Qual o valor da viagem?');
+        return;
+    }
+
+    if (user.andamento === 4) {
+        user.registroTemp.valor = `R$: ${originalMsg}`; // Usar a resposta original
+        user.andamento = 5;
+        client.sendMessage(message.from, 'Quantos passageiros?');
+        return;
+    }
+
+    // Quando o usuário envia a data
+    if (user.andamento === 5) {
+        user.registroTemp.passageiros = originalMsg; // Usar a resposta original
         user.agendamentos.push({ ...user.registroTemp }); // Adiciona o novo agendamento
         user.andamento = 0;
         user.registroTemp = {}; // Limpa o temporário
 
-        mandarAgenda(client, message, user);
 
         client.sendMessage(message.from, 'Agendamento realizado com sucesso!');
+
+        mandarAgenda(client, message, user);
+
         salvarAgendamentos();
         return;
     }
